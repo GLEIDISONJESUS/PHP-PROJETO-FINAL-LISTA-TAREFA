b2cb67c8951e7b695736d5496a517dbd2f8789cb
Aluno10 <Aluno10@DESKTOP-D6U12T8>
2020-01-13 21:51:52 -0200

    PROJETO FINAL LISTA DE TAREFAS

--- home.php
+++ home.php
@@ -1,17 +1,26 @@
 <?php
-include("classes/usuario.class.php");
-include("conexao.php");
+include ("classes/usuario.class.php");
+include ("classes/tarefa.class");
+include ("conexao.php");
 global $link;
 session_start();
 $usuario = $_SESSION["usuarioLogado"];
 $resultado = mysqli_query($link, "SELECT * FROM cadastro_usuarios.tarefa WHERE id_usuario = {$usuario->id}");
 $tarefas = array();
 if ($resultado) {
-	if ($resultado->num_rows > 0) {
-		while( $tarefa = mysqli_fetch_object($resultado) ) {
-			$tarefas[] = $tarefa;
-		}
-	}
+    if ($resultado->num_rows > 0) {
+        while ($tarefa = mysqli_fetch_object($resultado, 'Tarefa')) {
+            $tarefas[] = $tarefa;
+        }
+    }
+}
+if (isset($_SESSION["msgAdicionar"])) {
+    $msgAdicionar = $_SESSION["msgAdicionar"];
+    unset($_SESSION["msgAdicionar"]);
+}
+if (isset($_SESSION["msgProcessar"])) {
+    $msgProcessar = $_SESSION["msgProcessar"];
+    unset($_SESSION["msgProcessar"]);
 }
 ?>
 <!DOCTYPE html>
@@ -32,21 +41,45 @@ if ($resultado) {
 			</p>
 			<input type="submit" value="Adicionar" />
 		</form>
-		<?php if ( isset( $_SESSION["erroAdicionarTarefa"] ) ) {?>
-		<p>
-		<?php echo $_SESSION["erroAdicionarTarefa"]; ?>
-		</p>
-		<?php } else if ( isset( $_SESSION["sucessoAdicionarTarefa"] ) ) {?>
-		<p>
-		<?php echo $_SESSION["sucessoAdicionarTarefa"]; ?>
-		</p>
+		<?php if ( isset( $msgAdicionar ) ) {?>
+    		<p>
+    		<?php echo $msgAdicionar; ?>
+    		</p>
 		<?php } ?>
 	</fieldset>
 
 	<h2>Suas tarefas:</h2>
 
-	<?php foreach($tarefas as $t) { ?>
-	<p><?php echo $t->titulo; ?> ( <?php echo $t->finalizada ? "Finalizada" : "Em aberto";  ?> )</p>
-	<?php } ?>
+	<form method="POST" action="processar_tarefas.php">
+		<table>
+			<thead>
+				<tr>
+					<td></td>
+					<td>Tarefa</td>
+				</tr>
+			</thead>
+			<tbody>
+        		
+        	<?php foreach($tarefas as $t) { ?>
+        	<tr>
+					<td><input type="checkbox" name="tarefas[]"
+						value="<?php echo $t->id; ?>" /></td>
+					<td><?php echo $t->titulo; ?> ( <?php echo $t->finalizada ? "Finalizada" : "Em aberto";  ?> )</td>
+				</tr>
+        	<?php } ?>
+	
+		</tbody>
+		</table>
+		<select name="modo">
+			<option value="finalizar" selected>Finalizar</option>
+			<option value="excluir">Excluir</option>
+		</select> 
+		<input type="submit" value="Executar" />
+		<?php if ( isset( $msgProcessar ) ) {?>
+    		<p>
+    		<?php echo $msgProcessar; ?>
+    		</p>
+		<?php } ?>
+	</form>
 </body>
 </html>
